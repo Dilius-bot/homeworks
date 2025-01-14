189ab2b68146f106e1fdd10948a8467bfe41a71b
Dilius Gizetdinov <dilyus1997@mail.ru>
2025-01-14 19:27:32 +0300

    add exercise-8

--- exercise-8.js
+++ exercise-8.js
@@ -1,14 +1,20 @@
 const numberOfComputerOne = Math.floor(Math.random() * 100);
-let numberOfComputerTwo;
+let numberOfComputerTwo = 50;
+let start = 1;
+let end = 100;
 
 do {
-  numberOfComputerTwo = Math.floor(Math.random() * 100);
   console.log(`Компьютер 2: Пробую число ${numberOfComputerTwo}`);
   if (numberOfComputerTwo < numberOfComputerOne) {
     console.log("Компьютер 1: Больше");
+    start = numberOfComputerTwo;
+    numberOfComputerTwo = Math.floor(Math.random() * (end - start) + start);
   } else if (numberOfComputerTwo > numberOfComputerOne) {
     console.log("Компьютер 1: Меньше");
+    end = numberOfComputerTwo;
+    numberOfComputerTwo = Math.floor(Math.random() * (end - start) + start);
   }
 } while (numberOfComputerOne != numberOfComputerTwo);
 
+console.log(`Компьютер 2: Пробую число ${numberOfComputerTwo}`);
 console.log("Компьютер 1: Угадал!");
